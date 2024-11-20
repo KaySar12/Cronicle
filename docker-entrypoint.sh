90d257fcbf4f0a66f7b27c8e46acb96ba0dee038
hoangvv <shintheman12@gmail.com>
2024-11-20 14:12:35 +0700

    refactor cronicle entry check if data already setup use host.docker.internal instead of original hostname

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,10 @@ then
   echo "Running SLAVE server"
 else 
   echo "Running MASTER server"
-  /opt/cronicle/bin/control.sh setup
+  if [ ! -d "/opt/cronicle/data" ]; then
+    echo "Data directory doesn't exist, performing setup..."
+    /opt/cronicle/bin/control.sh setup
+  fi
 fi
 
 /opt/cronicle/bin/control.sh start
